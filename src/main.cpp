d183ae68bce94b20221359874255b9a57175e692
acetil <acetildev@gmail.com>
2019-08-03 14:08:19 +1000

    added window init to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "graphics/graphics_headers.h"
+#include "graphics/graphics_init.h"
+
 int main (int argv, char* argc[]) {
     GLFWwindow* window = NULL;
-
+    if (graphics::initWindow(&window) != GRAPHICS_INIT_SUCCESS) {
+        return GRAPHICS_INIT_FAILURE;
+    }
     return EXIT_SUCCESS;
 }
