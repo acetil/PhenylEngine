bb954deb469ca602a441c3359e7f312be48bc0d8
acetil <acetildev@gmail.com>
2019-08-04 09:05:01 +1000

    added initGraphics call

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,18 @@
 
 #include "graphics/graphics_headers.h"
 #include "graphics/graphics_init.h"
+#include "graphics.h"
+#include "logging/logging.h"
 
 int main (int argv, char* argc[]) {
     GLFWwindow* window = NULL;
     if (graphics::initWindow(&window) != GRAPHICS_INIT_SUCCESS) {
+        logging::log(LEVEL_INFO, "Window init failure, stopping!");
+        return GRAPHICS_INIT_FAILURE;
+    }
+    graphics::Graphics graphics = NULL;
+    if (graphics::initGraphics(window, &graphics) != GRAPHICS_INIT_SUCCESS) {
+        logging::log(LEVEL_INFO, "Graphics init failure, stopping!");
         return GRAPHICS_INIT_FAILURE;
     }
     return EXIT_SUCCESS;
