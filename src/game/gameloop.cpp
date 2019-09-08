3e0bc901a176d341082e53b898a11887616e72d0
acetil <acetildev@gmail.com>
2019-09-08 20:31:13 +1000

    started adding test entity controls

--- src/game/gameloop.cpp
+++ src/game/gameloop.cpp
@@ -5,12 +5,18 @@
 #include "graphics/graphics_headers.h"
 #include "logging/logging.h"
 #include "graphics/shaders/shaders.h"
+#include "key_input.h"
+#include "key_defaults.h"
+
 
 using namespace game; 
 
 int game::gameloop (graphics::Graphics* graphics) {
     GameObject* gameObject = initGame(graphics);
-    gameObject->createNewEntityInstance("test_entity", 0, 0);
+    AbstractEntity* player = gameObject->createNewEntityInstance("test_entity", 0, 0);
+    KeyboardInput* keyInput = new KeyboardInput(graphics);
+    float deltaTime = 0.0f;
+    
     while (!graphics->shouldClose()) {
         //gameObject->updateEntities();
         //gameObject->updateEntityPositions();
