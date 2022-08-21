0a6392d310028310ba6809b7d661dff8b017f7e5
acetil <acetildev@gmail.com>
2022-08-21 12:24:20 +1000

    Added bitfield class (a bit incomplete)

---
+++ modules/util/include/util/bitfield.h
@@ -0,0 +1,172 @@
+#pragma once
+
+#include <cstddef>
+#include <cassert>
+#include <cstdint>
+
+namespace util {
+    namespace detail {
+        static constexpr std::size_t ceil (std::size_t val, std::size_t denom) {
+            return val / denom + (val % denom ? 1 : 0);
+        }
+    }
+    template <std::size_t N>
+    class Bitfield {
+    private:
+        static constexpr std::size_t FullMask = ~((std::size_t)0);
+        static constexpr std::size_t IntSize = sizeof(size_t) * 8;
+        static constexpr std::size_t FullInts = N / (IntSize);
+        static constexpr std::size_t NumInts = detail::ceil(N, IntSize);
+
+        std::size_t data[NumInts];
+    public:
+        Bitfield () : data{{0}} {}
+
+        Bitfield (const Bitfield<N>& other) : data{other.data} {}
+        Bitfield<N>& operator= (const Bitfield<N>& other) {
+            if (this == &other) {
+                return *this;
+            }
+            data = other.data;
+
+            return *this;
+        }
+
+        Bitfield (Bitfield<N>&& other) noexcept : data{other} {}
+        Bitfield<N>& operator= (Bitfield<N>&& other) noexcept {
+            data = other.data;
+
+            return *this;
+        }
+
+        static Bitfield<N> Mask (std::size_t numBits) {
+            assert(numBits <= N);
+            Bitfield<N> result;
+
+            std::size_t index = 0;
+            while (index * IntSize < numBits) {
+                if ((index + 1) * IntSize <= numBits) {
+                    result.data[index] = FullMask;
+                } else {
+                    result.data[index] = FullMask & ((1 << (numBits - index * IntSize)) - 1);
+                }
+                index++;
+            }
+
+            return result;
+        }
+
+        bool hasBit (std::size_t bit) {
+            if (bit >= N) {
+                return false;
+            }
+
+            return data[bit / IntSize] & (1 << (bit % IntSize));
+        }
+
+        void putBit (std::size_t bit) {
+            assert(bit < N);
+
+            data[bit / IntSize] |= 1 << (bit % IntSize);
+        }
+
+        void maskBit (std::size_t bit) {
+            assert(bit < N);
+
+            data[bit / IntSize] &= (FullMask ^ (1 << (bit % IntSize)));
+        }
+
+        operator bool () {
+            for (std::size_t i = 0; i < NumInts; i++) {
+                if (data[i]) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        Bitfield<N> operator~ () {
+            Bitfield<N> other{};
+            for (std::size_t i = 0; i < FullInts; i++) {
+                other.data[i] = ~data[i];
+            }
+
+            if constexpr (NumInts > FullInts) {
+                other.data[NumInts - 1] = (~data[NumInts - 1]) & ((1 << (N % IntSize)) - 1);
+            }
+
+            return other;
+        }
+
+        Bitfield<N>& operator&= (const Bitfield<N>& other) {
+            for (std::size_t i = 0; i < NumInts; i++) {
+                data[i] &= other.data[i];
+            }
+
+            return *this;
+        }
+
+        Bitfield<N>& operator|= (const Bitfield<N>& other) {
+            for (std::size_t i = 0; i < NumInts; i++) {
+                data[i] |= other.data[i];
+            }
+
+            return *this;
+        }
+
+        Bitfield<N>& operator^= (const Bitfield<N>& other) {
+            for (std::size_t i = 0; i < NumInts; i++) {
+                data[i] ^= other.data[i];
+            }
+
+            return *this;
+        }
+
+        friend bool operator== (const Bitfield<N>& bitfield1, const Bitfield<N>& bitfield2);
+        friend Bitfield<N> operator& (const Bitfield<N>& bitfield1, const Bitfield<N>& bitfield2);
+        friend Bitfield<N> operator| (const Bitfield<N>& bitfield1, const Bitfield<N>& bitfield2);
+        friend Bitfield<N> operator^ (const Bitfield<N>& bitfield1, const Bitfield<N>& bitfield2);
+    };
+
+    template <std::size_t N>
+    inline bool operator== (const Bitfield<N>& bitfield1, const Bitfield<N>& bitfield2) {
+        for (std::size_t i = 0; i < Bitfield<N>::NumInts; i++) {
+            if (bitfield1.data[i] != bitfield2.data[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    template <std::size_t N>
+    inline Bitfield<N> operator& (const Bitfield<N>& bitfield1, const Bitfield<N>& bitfield2) {
+        Bitfield<N> result{};
+        for (std::size_t i = 0; i < Bitfield<N>::NumInts; i++) {
+            result.data[i] = bitfield1.data[i] & bitfield2.data[i];
+        }
+
+        return result;
+    }
+
+    template <std::size_t N>
+    inline Bitfield<N> operator| (const Bitfield<N>& bitfield1, const Bitfield<N>& bitfield2) {
+        Bitfield<N> result{};
+        for (std::size_t i = 0; i < Bitfield<N>::NumInts; i++) {
+            result.data[i] = bitfield1.data[i] | bitfield2.data[i];
+        }
+
+        return result;
+    }
+
+    template <std::size_t N>
+    inline Bitfield<N> operator^ (const Bitfield<N>& bitfield1, const Bitfield<N>& bitfield2) {
+        Bitfield<N> result{};
+        for (std::size_t i = 0; i < Bitfield<N>::NumInts; i++) {
+            result.data[i] = bitfield1.data[i] ^ bitfield2.data[i];
+        }
+
+        return result;
+    }
+}
